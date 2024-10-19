09664881ea147bb10597c36bccfcb0435a4702a2
dangthanhphuc2512 <dangthanhphuc2512@gmail.com>
2024-10-20 01:27:23 +0700

    Completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { SearchSectionComponent } from './components/search-section/search-section.component';
+import { NotificationSectionComponent } from './components/notification-section/notification-section.component';
+import { PersonalPageSectionComponent } from './components/personal-page-section/personal-page-section.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+
+    { path: 'homepage', component: HomepageComponent },
+    { path: 'search-section', component: SearchSectionComponent },
+    { path: 'notification-section', component: NotificationSectionComponent },
+    { path: 'personal-page-section', component: PersonalPageSectionComponent },
+
+    { path: '**', component: HomepageComponent }
+];
